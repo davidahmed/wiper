c5ed841bc4a92be800b33dd5cfe3470a4330ff8e
david.ahmed <davedshepherd@gmail.com>
2020-05-23 19:55:49 +0200

    Added a simple blacklist to remove website search results from blacklist

--- wiper.js
+++ wiper.js
@@ -1,3 +1,12 @@
 document.body.style.border = "5px solid red";
 console.log("Target available");
 
+var res = document.getElementsByClassName('r')
+
+var blacklist = new RegExp('https?:\/\/.*\.(geeksforgeeks|tutorialspoint).*\.*');
+
+for (var i= 0; i < res.length; i++){
+	if(res[i].childNodes[0].getAttribute('href').match(blacklist)){
+		res[i].parentElement.remove()
+	}
+}
