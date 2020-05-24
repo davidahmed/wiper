22bf2e0f3c1cff7eef2ad4bbc614819e5933c79c
david.ahmed <davedshepherd@gmail.com>
2020-05-24 08:10:48 +0200

    Fixed the bug where the relevant search results were not being removed successfully.

--- sidebar/panel.js
+++ sidebar/panel.js
@@ -6,9 +6,18 @@ function pushURL(urls, newURL){
 	browser.storage.local.set({'blacklistURLs':oldURLs});
 };
 
+function popURL(urls, urlToRemove){
+	var oldURLs = urls.blacklistURLs;
+	const index = oldURLs.indexOf(urlToRemove);
+	if (index > -1){
+		oldURLs.splice(index, 1);
+	}
+	browser.storage.local.set({'blacklistURLs':oldURLs});
+}
 
 async function addURL(){
 	var url = document.getElementById('inputURL').value;
+	document.getElementById('inputURL').value = "";
 	if (typeof url.match(URLregex)[3] !== 'undefined'){
 		await browser.storage.local.get("blacklistURLs").then(function(urls){
 			return pushURL(urls, url.match(URLregex)[3])});
@@ -16,6 +25,15 @@ async function addURL(){
 	browser.storage.local.get("blacklistURLs").then(displayURLs);
 }
 
+async function removeURL(event){
+	var urlToRemove = event.target.parentElement.innerText;
+	if (typeof urlToRemove != undefined){
+		await browser.storage.local.get('blacklistURLs').then(function(urls){
+			return popURL(urls, urlToRemove)});
+	}
+	browser.storage.local.get("blacklistURLs").then(displayURLs);
+
+}
 
 function displayURLs(urls){
 	urls = urls.blacklistURLs;
@@ -24,8 +42,17 @@ function displayURLs(urls){
 		document.getElementById('urls').textContent = '';
 		for (var i=0; i<urls.length; i++){
 			var e = document.createElement('div');
+			var removeButton = document.createElement('input');
+
 			e.className = 'urlEntry';
 			e.innerHTML = urls[i];
+
+			removeButton.className = 'removeButton';
+			removeButton.value = 'Remove';
+			removeButton.type = 'button';
+			removeButton.onclick = removeURL;
+
+			e.appendChild(removeButton);
 			document.getElementById('urls').appendChild(e);
 		}
 	}
--- wiper.js
+++ wiper.js
@@ -6,17 +6,21 @@ var blacklist = new RegExp('https?:\/\/.*\.(geeksforgeeks|tutorialspoint).*\.*')
 
 
 function clearURLs(urls){
-	var res = document.getElementsByClassName('r')
+	var arr = [];
+	var res = document.querySelectorAll('div.rc');
 	console.log(res);
 	console.log(urls.blacklistURLs);
 
 	for (var i= 0; i < res.length; i++){
 		for (var j=0; j < urls.blacklistURLs.length; j++){
-			if(res[i].childNodes[0].getAttribute('href').indexOf(urls.blacklistURLs[j]) !== -1){
-				res[i].parentElement.remove();
+			if(res[i].firstChild.firstChild.getAttribute('href').indexOf(urls.blacklistURLs[j]) !== -1){
+				arr.push(i);
 			}
 		}
 	}
+	arr = [...new Set(arr)];
+	console.log(arr);
+	for(var i=0; i<arr.length; i++){res[arr[i]].parentElement.innerHTML=""}
 }
 
 browser.storage.local.get('blacklistURLs').then(clearURLs);
