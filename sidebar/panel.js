818640891624ad75f2f61a1aaac71179fedb4926
david.ahmed <davedshepherd@gmail.com>
2020-05-24 19:46:27 +0200

    Linting with JSLint

--- sidebar/panel.js
+++ sidebar/panel.js
@@ -1,62 +1,63 @@
-const URLregex = new RegExp('((ftp|http|https):\/\/)?([a-zA-Z1-9@:%._\+~#=\.\-]+)(\/.*)?');
+var URLregex = new RegExp('((ftp|http|https):\/\/)?([a-zA-Z1-9@:%._\+~#=\.\-]+)(\/.*)?');
 
-function pushURL(urls, newURL){
-	var oldURLs = urls.blacklistURLs;
-	oldURLs.push(newURL);
-	browser.storage.local.set({'blacklistURLs':oldURLs});
-};
-
-function popURL(urls, urlToRemove){
-	var oldURLs = urls.blacklistURLs;
-	const index = oldURLs.indexOf(urlToRemove);
-	if (index > -1){
-		oldURLs.splice(index, 1);
-	}
-	browser.storage.local.set({'blacklistURLs':oldURLs});
+function pushURL(urls, newURL) {
+    var oldURLs = urls.blacklistURLs;
+    oldURLs.push(newURL);
+    browser.storage.local.set({'blacklistURLs': oldURLs});
 }
 
-async function addURL(){
-	var url = document.getElementById('addURLField').value;
-	document.getElementById('addURLField').value = "";
-	if (typeof url.match(URLregex)[3] !== 'undefined'){
-		await browser.storage.local.get("blacklistURLs").then(function(urls){
-			return pushURL(urls, url.match(URLregex)[3])});
-	}
-	browser.storage.local.get("blacklistURLs").then(displayURLs);
+function popURL(urls, urlToRemove) {
+    var oldURLs = urls.blacklistURLs,
+        index = oldURLs.indexOf(urlToRemove);
+
+    if (index > -1) {
+        oldURLs.splice(index, 1);
+    }
+    browser.storage.local.set({'blacklistURLs': oldURLs});
 }
 
-async function removeURL(event){
-	var urlToRemove = event.target.parentElement.innerText;
-	if (typeof urlToRemove != undefined){
-		await browser.storage.local.get('blacklistURLs').then(function(urls){
-			return popURL(urls, urlToRemove)});
-	}
-	browser.storage.local.get("blacklistURLs").then(displayURLs);
-
+async function addURL() {
+    var url = document.getElementById('addURLField').value;
+    document.getElementById('addURLField').value = "";
+    if (typeof url.match(URLregex)[3] !== 'undefined') {
+        await browser.storage.local.get("blacklistURLs").then(function (urls) {
+            return pushURL(urls, url.match(URLregex)[3]);
+        });
+    }
+    browser.storage.local.get("blacklistURLs").then(displayURLs);
 }
 
-function displayURLs(urls){
-	urls = urls.blacklistURLs;
-	if (urls !== 'undefined'){
-		document.getElementById('urls').textContent = '';
-		for (var i=0; i<urls.length; i++){
-			var e = document.createElement('div');
-			var removeButton = document.createElement('input');
-
-			e.className = 'urlEntry';
-			e.innerHTML = urls[i];
-
-			removeButton.className = 'removeButton';
-			removeButton.value = 'Remove';
-			removeButton.type = 'button';
-			removeButton.onclick = removeURL;
-
-			e.appendChild(removeButton);
-			document.getElementById('urls').appendChild(e);
-		}
-	}
+async function removeURL(event) {
+    var urlToRemove = event.target.parentElement.innerText;
+    if (typeof urlToRemove != undefined) {
+        await browser.storage.local.get('blacklistURLs').then(function (urls) {
+            return popURL(urls, urlToRemove);
+        });
+    }
+    browser.storage.local.get("blacklistURLs").then(displayURLs);
 }
 
+function displayURLs(urls) {
+    var i, e, removeButton;
+    urls = urls.blacklistURLs;
+    if (urls !== 'undefined') {
+        document.getElementById('urls').textContent = '';
+        for (i = 0; i < urls.length; i++) {
+            e = document.createElement('div');
+            removeButton = document.createElement('input');
+            e.className = 'urlEntry';
+            e.innerHTML = urls[i];
+
+            removeButton.className = 'removeButton';
+            removeButton.value = 'Remove';
+            removeButton.type = 'button';
+            removeButton.onclick = removeURL;
+
+            e.appendChild(removeButton);
+            document.getElementById('urls').appendChild(e);
+        }
+    }
+}
 
 browser.storage.local.get('blacklistURLs').then(displayURLs);
 document.getElementById("addURLButton").addEventListener("click", addURL);
--- wiper.js
+++ wiper.js
@@ -2,25 +2,25 @@ document.body.style.border = "1px solid blue";
 
 var blacklist = new RegExp('https?:\/\/.*\.(geeksforgeeks|tutorialspoint).*\.*');
 
+function clearURLs(urls) {
+    var i, j, arr, res, url;
+    arr = [];
+    res = document.querySelectorAll('div.rc');
 
+    for (i = 0; i < res.length; i++) {
+        for (j = 0; j < urls.blacklistURLs.length; j++) {
+            if (res[i].firstChild.firstChild.getAttribute('href').indexOf(urls.blacklistURLs[j]) !== -1) {
+                arr.push(i);
+            }
+        }
+    }
+    arr = [...new Set(arr)];
 
-function clearURLs(urls){
-	var arr = [];
-	var res = document.querySelectorAll('div.rc');
-
-	for (var i= 0; i < res.length; i++){
-		for (var j=0; j < urls.blacklistURLs.length; j++){
-			if(res[i].firstChild.firstChild.getAttribute('href').indexOf(urls.blacklistURLs[j]) !== -1){
-				arr.push(i);
-			}
-		}
-	}
-	arr = [...new Set(arr)];
-	for(var i=0; i<arr.length; i++){
-		var url = res[arr[i]].firstChild.firstChild.getAttribute('href');
-		url = 'Wiper blacklisted URL: <a href="'+url+'">'+url+'</a>';
-		res[arr[i]].parentElement.innerHTML = url;
-	}
+    for (i = 0; i < arr.length; i++) {
+        url = res[arr[i]].firstChild.firstChild.getAttribute('href');
+        url = 'Wiper blacklisted URL: <a href="' + url + '">' + url + '</a>';
+        res[arr[i]].parentElement.innerHTML = url;
+    }
 }
 
 browser.storage.local.get('blacklistURLs').then(clearURLs);
