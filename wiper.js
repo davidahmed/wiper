84e699ae1dab6c1680a9c26119fc38c9a9c41fd4
david.ahmed <davedshepherd@gmail.com>
2020-05-31 01:38:41 +0200

    Refactor clearURLs to make it simpler and more readable

--- wiper.js
+++ wiper.js
@@ -1,26 +1,25 @@
-document.body.style.border = "1px solid blue";
-
-var blacklist = new RegExp('https?:\/\/.*\.(geeksforgeeks|tutorialspoint).*\.*');
-
+/**
+ * Modify the search results on (Google.com) that happen to be
+ * from certain URLs.
+ * @param {Array} urls- a list of URLs to modify the results of 
+ */
 function clearURLs(urls) {
-    var i, j, arr, res, url;
-    arr = [];
-    res = document.querySelectorAll('div.rc');
+  const isBlacklistedUrl = (link) => urls.some(url => link.includes(url));
+  const decorateURL = (url) => `Wiper blacklisted URL: <a href="${url}">${url}</a>`;
 
-    for (i = 0; i < res.length; i++) {
-        for (j = 0; j < urls.blacklistURLs.length; j++) {
-            if (res[i].firstChild.firstChild.getAttribute('href').indexOf(urls.blacklistURLs[j]) !== -1) {
-                arr.push(i);
-            }
-        }
-    }
-    arr = [...new Set(arr)];
+  Array.from(document.querySelectorAll('div.g'))
+  filter(result => isBlacklistedUrl(result.querySelector('a').href))
+  forEach(result => {result.innerHTML = decorateURL(result.querySelector('a').href)});
 
-    for (i = 0; i < arr.length; i++) {
-        url = res[arr[i]].firstChild.firstChild.getAttribute('href');
-        url = 'Wiper blacklisted URL: <a href="' + url + '">' + url + '</a>';
-        res[arr[i]].parentElement.innerHTML = url;
-    }
+  return true;
 }
 
-browser.storage.local.get('blacklistURLs').then(clearURLs);
+
+function handleBlacklistURLs(blacklist){
+    return clearURLs(blacklist.blacklistURLs);
+  }
+    
+
+/* global operations for every time the target page (./google./search) loads */
+document.body.style.border = "1px solid blue";
+browser.storage.local.get('blacklistURLs').then(handleBlacklistURLs);
